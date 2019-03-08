ad193ca8f9f28f8c8b784bba5398e5f895dd5657
Hugo Hörnquist <hugo@lysator.liu.se>
2019-03-08 21:54:50 +0100

    Simplify srfi-19 setters.

--- srfi/srfi-19/setters.scm
+++ srfi/srfi-19/setters.scm
@@ -4,12 +4,12 @@
   #:export (nanosecond second minute hour day month year zone-offset))
 
 
-(define nanosecond (make-procedure-with-setter (@ (srfi srfi-19) date-nanosecond) (@@ (srfi srfi-19) set-date-nanosecond!)))
-(define second (make-procedure-with-setter (@ (srfi srfi-19) date-second) (@@ (srfi srfi-19) set-date-second!)))
-(define minute (make-procedure-with-setter (@ (srfi srfi-19) date-minute) (@@ (srfi srfi-19) set-date-minute!)))
-(define hour (make-procedure-with-setter (@ (srfi srfi-19) date-hour) (@@ (srfi srfi-19) set-date-hour!)))
-(define day (make-procedure-with-setter (@ (srfi srfi-19) date-day) (@@ (srfi srfi-19) set-date-day!)))
-(define month (make-procedure-with-setter (@ (srfi srfi-19) date-month) (@@ (srfi srfi-19) set-date-month!)))
-(define year (make-procedure-with-setter (@ (srfi srfi-19) date-year) (@@ (srfi srfi-19) set-date-year!)))
-(define zone-offset (make-procedure-with-setter (@ (srfi srfi-19) date-zone-offset) (@@ (srfi srfi-19) set-date-zone-offset!)))
+(define nanosecond (make-procedure-with-setter date-nanosecond (@@ (srfi srfi-19) set-date-nanosecond!)))
+(define second (make-procedure-with-setter date-second (@@ (srfi srfi-19) set-date-second!)))
+(define minute (make-procedure-with-setter date-minute (@@ (srfi srfi-19) set-date-minute!)))
+(define hour (make-procedure-with-setter date-hour (@@ (srfi srfi-19) set-date-hour!)))
+(define day (make-procedure-with-setter date-day (@@ (srfi srfi-19) set-date-day!)))
+(define month (make-procedure-with-setter date-month (@@ (srfi srfi-19) set-date-month!)))
+(define year (make-procedure-with-setter date-year (@@ (srfi srfi-19) set-date-year!)))
+(define zone-offset (make-procedure-with-setter date-zone-offset (@@ (srfi srfi-19) set-date-zone-offset!)))
 
