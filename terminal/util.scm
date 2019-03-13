4a917940b749719650d1a7329e9ac7ac8e5ce5cd
Hugo Hörnquist <hugo@lysator.liu.se>
2019-03-13 15:33:17 +0100

    Fix BSD cal output.

--- terminal/util.scm
+++ terminal/util.scm
@@ -14,8 +14,11 @@
   (let* ((day   (number->string (date-day   date)))
          (month (number->string (date-month date)))
          (year  (number->string (date-year  date))))
+    ;; BSD cal only supports setting highlighted day explicitly for
+    ;; testing the functionality. This seems to at least give me
+    ;; an (almost) working display, albeit ugly.
     (if (file-exists? "/usr/bin/ncal")
-        (system*  "cal" "-3" "-H" (format #f "~a-~a-~a"
+        (system*  "ncal" "-3" "-H" (format #f "~a-~a-~a"
                                           year month day)
                   month year)
         (system* "cal" "-3" day month year))))
