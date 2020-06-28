c36b4fc8c4f1a1d43bbd66867c36a1c25a74d455
Hugo Hörnquist <hugo@lysator.liu.se>
2020-06-29 01:08:11 +0200

    Allow multiple equals in HTTP queries.

--- module/server/util.scm
+++ module/server/util.scm
@@ -5,6 +5,10 @@
 (define-public (parse-query query)
   (when query
     (fold (lambda (str list)
-            (let* (((k v) (string-split str #\=)))
-              (cons* (-> k string->symbol symbol->keyword) v list)))
+            ;; only split on the first equal.
+            ;; Does HTTP allow multiple equal signs in a data field?
+            (define idx (string-index str #\=))
+            (define key (substring str 0 idx))
+            (define val (substring str (1+ idx)))
+            (cons* (-> key string->symbol symbol->keyword) val list))
           '() (string-split query #\&))))
