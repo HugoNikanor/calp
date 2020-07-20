f4c7b0224f8d17149633dd826f6e506f796b6999
Hugo Hörnquist <hugo@lysator.liu.se>
2020-07-20 03:23:40 +0200

    Add preliminary tests for cpp.

---
+++ tests/cpp.scm
@@ -0,0 +1,36 @@
+(define run (compose parse-lexeme-tree lex))
+
+(test-equal
+    '(+ (post-increment (dereference C)) 3)
+  (run "(*C)++ + 3"))
+
+
+(test-equal
+    '(+ (post-increment (dereference C)) 3)
+  (run "*C++ + 3"))
+
+(test-equal
+    '(post-increment (dereference C))
+  (run "*C++"))
+
+(test-equal
+    '(+ (post-increment C) (post-increment C))
+  (run "C++ + C++"))
+
+(test-equal
+    '(+ (pre-increment C) (pre-increment C))
+  (run "++C + ++C"))
+
+
+(test-equal
+    '(+ 2 (* 2 2))
+  (run "2 + 2 * 2"))
+
+(test-equal
+    '(+ (* 2 2) 2)
+  (run "2 * 2 + 2"))
+
+(test-equal
+    '(+ 2 2 2)
+  (run "2+2+2"))
+
