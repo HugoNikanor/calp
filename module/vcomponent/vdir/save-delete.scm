08d4886a82ccc371dac91ec1906fb99dcc1a828f
Hugo Hörnquist <hugo@lysator.liu.se>
2020-08-17 18:25:54 +0200

    Errors not correct in this context.

--- module/vcomponent/vdir/save-delete.scm
+++ module/vcomponent/vdir/save-delete.scm
@@ -17,39 +17,23 @@
   )
 
 
-(define / file-name-separator-string)
-
 (define-public (save-event event)
   (define calendar (parent event))
-  (case (prop calendar '-X-HNH-SOURCETYPE)
-    [(file)
-     (error "Importing into direct calendar files not supported")]
 
-    [(vdir)
-     (let* ((uid (or (prop event 'UID) (uuidgen))))
-       (set! (prop event 'UID) uid
-             ;; TODO use existing filename if present?
-             (prop event '-X-HNH-FILENAME) (string-append
-                                            (prop calendar '-X-HNH-DIRECTORY)
-                                            / uid ".ics"))
-       (with-atomic-output-to-file (prop event '-X-HNH-FILENAME)
-        (lambda () (print-components-with-fake-parent (list event))))
-       uid)]
+  (assert 'vdir (prop calendar '-X-HNH-SOURCETYPE))
 
-    [else
-     (error "Source of calendar unknown, aborting.")
-     ]))
+  (let* ((uid (or (prop event 'UID) (uuidgen))))
+    (set! (prop event 'UID) uid
+      ;; TODO use existing filename if present?
+      (prop event '-X-HNH-FILENAME) (path-append
+                                      (prop calendar '-X-HNH-DIRECTORY)
+                                      (string-append uid ".ics")))
+    (with-atomic-output-to-file (prop event '-X-HNH-FILENAME)
+      (lambda () (print-components-with-fake-parent (list event))))
+    uid))
 
 
 (define-public (remove-event event)
   (define calendar (parent event))
-  (case (prop calendar '-X-HNH-SOURCETYPE)
-    [(file)
-     (error "Removing events from large files unsupported")]
-
-    [(vdir)
-     (delete-file (prop event '-X-HNH-FILENAME))]
-
-    [else
-     (error "Source of calendar unknown, aborting.")
-     ]))
+  (assert 'vdir (prop calendar '-X-HNH-SOURCETYPE))
+  (delete-file (prop event '-X-HNH-FILENAME)))
