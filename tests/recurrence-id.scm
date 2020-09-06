6166728792c65ff1d24238330734bfc0c7ecf829
Hugo Hörnquist <hugo@lysator.liu.se>
2020-09-06 19:01:32 +0200

    Add test for Reccurence-ID with different DTSTART type.

---
+++ tests/recurrence-id.scm
@@ -0,0 +1,30 @@
+(((srfi srfi-41) stream->list)
+ ((vcomponent) parse-calendar)
+ ((vcomponent recurrence) generate-recurrence-set)
+ ((guile) format)
+ )
+
+(define uid (symbol->string (gensym "areallyuniqueid")))
+
+(define ev
+ (call-with-input-string
+     (format #f "BEGIN:VCALENDAR
+BEGIN:VEVENT
+SUMMARY:Changing type on Recurrence-id.
+UID:~a
+DTSTART;VALUE=DATE:20090127
+END:VEVENT
+BEGIN:VEVENT
+UID:~a
+SUMMARY:Changing type on Recurrence-id.
+DTSTART;TZID=Europe/Stockholm:20100127T120000
+RECURRENCE-ID;VALUE=DATE:20100127
+SUMMARY:This instance only has a time component
+END:VEVENT
+END:VCALENDAR"
+             uid uid)
+   parse-calendar))
+
+
+(test-assert "Changing type on Recurrence id."
+  (stream->list 10 (generate-recurrence-set ev)))
