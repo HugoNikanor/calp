e53a507fd464d0dba069b193027d891bd3da61b2
Hugo Hörnquist <hugo@lysator.liu.se>
2021-01-13 22:43:45 +0100

    Add additional parse-endpoint-string tests.

--- tests/server.scm
+++ tests/server.scm
@@ -2,7 +2,20 @@
 ;; Tests parse-endpoint-string, used for defining server routes.
 ;;; Code:
 
-(test-assert (parse-endpoint-string "/static/:dir/:file"))
-;; => "/static/([^/]+)/([^/]+)"
-;; => (dir file)
+(((web http make-routes) parse-endpoint-string)
+ ((calp util) let*))
 
+(test-assert "Check that parsing doesn't crash"
+  (parse-endpoint-string "/static/:dir/:file"))
+
+;; Checks that parsing produces correct results
+(let* ((path args (parse-endpoint-string "/static/:dir/:file")))
+  (test-equal "/static/([^/]+)/([^/]+)" path)
+  (test-equal '(dir file) args))
+
+
+;; Checks that parsing with custom regex works
+;; along with literal periods.
+(let* ((path args (parse-endpoint-string "/static/:filename{.*}.:ext")))
+  (test-equal "/static/(.*)\\.([^/.]+)" path)
+  (test-equal '(filename ext) args))
