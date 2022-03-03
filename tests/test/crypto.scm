360691121ed49e3b684a4492c17e61e311de5087
Hugo Hörnquist <hugo@lysator.liu.se>
2022-03-03 02:29:08 +0100

    Add tests for (crypto) module.

---
+++ tests/test/crypto.scm
@@ -0,0 +1,9 @@
+(use-modules ((crypto) :select (sha256 checksum->string)))
+
+(test-equal "sha256"
+  #vu8(24 95 141 179 34 113 254 37 245 97 166 252 147 139 46 38 67 6 236 48 78 218 81 128 7 209 118 72 38 56 25 105)
+  (sha256 "Hello"))
+
+(test-equal "sha256 string digest"
+  "185f8db32271fe25f561a6fc938b2e264306ec304eda518007d1764826381969"
+  (checksum->string (sha256 "Hello")))
