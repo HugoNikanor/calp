2096b972976b68750dadbed9c159c62fc3ac1678
Hugo Hörnquist <hugo@lysator.liu.se>
2022-03-06 21:15:00 +0100

    Cleanup in (hnh util path).

--- module/hnh/util/path.scm
+++ module/hnh/util/path.scm
@@ -2,30 +2,37 @@
   :use-module (srfi srfi-1)
   :use-module (hnh util))
 
+(define // file-name-separator-string)
+(define /? file-name-separator?)
+
 (define-public (path-append . strings)
   (fold (lambda (s done)
-            (string-append
-             done
-             (if (string-null? s)
-                 (string-append s file-name-separator-string)
-                 (if (file-name-separator? (string-last done))
-                     (if (file-name-separator? (string-first s))
-                         (string-drop s 1) s)
-                     (if (file-name-separator? (string-first s))
-                         s (string-append file-name-separator-string s))))))
+          (string-append
+           done
+           (cond ((string-null? s) //)
+                 ((and (/? (string-first s))
+                       (/? (string-last done)))
+                  (string-drop s 1))
+                 ((or (/? (string-first s))
+                      (/? (string-last done)))
+                  s)
+                 (else (string-append // s)))))
         ;; If first component is empty, add a leading slash to make
         ;; the path absolute. This isn't exactly correct if we have
         ;; drive letters, but on those system the user should make
         ;; sure that the first component of the path is non-empty.
         (let ((s (car strings)))
           (if (string-null? s)
-              file-name-separator-string s))
-        (cdr strings)))
+              // s))
+        (cdr strings)
+        ))
 
 (define-public (path-join lst) (apply path-append lst))
 
 ;; @example
 ;; (path-split "usr/lib/test")
+;; ⇒ ("usr" "lib" "test")
+;; (path-split "usr/lib/test/")
 ;; ⇒ ("usr" "lib" "test")
 ;; (path-split "/usr/lib/test")
 ;; ⇒ ("" "usr" "lib" "test")
@@ -38,7 +45,7 @@
                 (reverse
                  (map reverse-list->string
                       (fold (lambda (c done)
-                              (if (file-name-separator? c)
+                              (if (/? c)
                                   (cons '() done)
                                   (cons (cons c (car done)) (cdr done))))
                             '(())
