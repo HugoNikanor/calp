32175fb935683d00253a875e65dcb5bf498de15c
Hugo Hörnquist <hugo@lysator.liu.se>
2022-04-06 23:02:14 +0200

    split (all-modules) into two procedures.

--- scripts/all-modules.scm
+++ scripts/all-modules.scm
@@ -3,12 +3,9 @@
   :use-module (srfi srfi-1)
   :use-module (ice-9 ftw)
   :use-module (ice-9 match)
-  :export (all-modules-under-directory))
+  :export (all-files-and-modules-under-directory all-modules-under-directory))
 
-(define (all-modules-under-directory dir)
-  "Returns two values, all scm files in dir, and all top
-level modules in those files"
-
+(define (all-files-and-modules-under-directory dir)
   (define re (make-regexp "\\.scm$"))
 
   (define files '())
@@ -23,11 +20,19 @@ level modules in those files"
                    (else #t))))
 
 
-  (values files
-          (filter identity
-                  (map (lambda (file)
-                         (match (call-with-input-file file read)
-                           (('define-module (module ...) _ ...)
-                            module)
-                           (_ #f)))
-                       files))))
+  (map (lambda (file)
+         (list file
+               (match (call-with-input-file file read)
+                 (('define-module (module ...) _ ...)
+                  module)
+                 (_ #f))))
+       files))
+
+(define (all-modules-under-directory dir)
+  "Returns two values, all scm files in dir, and all top
+level modules in those files"
+
+  (define pairs (all-files-and-modules-under-directory dir))
+  (values
+   (map car pairs)
+   (filter identity (map cadr pairs))))
