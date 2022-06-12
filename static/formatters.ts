7ee9a976548a290735bb9191c7541c3229d01926
Hugo Hörnquist <hugo@lysator.liu.se>
2022-06-12 03:12:25 +0200

    Ensure datetime gets set for generated time tags.

--- static/formatters.ts
+++ static/formatters.ts
@@ -6,11 +6,11 @@ import { makeElement } from './lib'
 
 declare global {
     interface Window {
-        formatters : Map<string, (e : HTMLElement, s : any) => void>;
+        formatters: Map<string, (e: HTMLElement, s: any) => void>;
     }
 }
 
-let formatters : Map<string, (e : HTMLElement, s : any) => void>;
+let formatters: Map<string, (e: HTMLElement, s: any) => void>;
 formatters = window.formatters = new Map();
 
 
@@ -18,12 +18,33 @@ formatters.set('categories', (el, d) => {
     for (let item of d) {
         let q = encodeURIComponent(
             `(member "${item}" (or (prop event (quote CATEGORIES)) (quote ())))`)
-            el.appendChild(makeElement('a', {
-                textContent: item,
-                href: `/search/?q=${q}`,
-            }))
+        el.appendChild(makeElement('a', {
+            textContent: item,
+            href: `/search/?q=${q}`,
+        }))
     }
 })
+
+function format_time_tag(el: HTMLElement, d: any): void {
+    if (el instanceof HTMLTimeElement) {
+        if (d instanceof Date) {
+            let fmt = '';
+            if (!d.utc) {
+                fmt += '~L';
+            }
+            fmt += '~Y-~m-~d'
+            if (!d.dateonly) {
+                fmt += 'T~H:~M:~S'
+            }
+            el.dateTime = d.format(fmt);
+        }
+    }
+
+    formatters.get('default')!(el, d);
+}
+
+formatters.set('dtstart', format_time_tag)
+formatters.set('dtend', format_time_tag)
 
 formatters.set('default', (el, d) => {
     let fmt;
