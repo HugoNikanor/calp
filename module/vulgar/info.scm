118c8c6d3fe5270adf47d34b852df649128d3bea
Hugo Hörnquist <hugo@lysator.liu.se>
2022-06-12 21:16:01 +0200

    Add explaining comment to get-terminal-size.

--- module/vulgar/info.scm
+++ module/vulgar/info.scm
@@ -1,6 +1,8 @@
 (define-module (vulgar info)
   :use-module (hnh util))
 
+;; Sort-of backwards subprocess call since we want the current terminal to be
+;; inherited by stty
 (define-public (get-terminal-size)
  (let* (((rpipe . wpipe) (pipe)))
    (system (format #f "stty size > /proc/~s/fd/~s"
