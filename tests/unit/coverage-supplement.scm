cd66e6068ca65a4ee15ea04d7342eccbe4c565a5
Hugo Hörnquist <hugo@lysator.liu.se>
2023-10-08 11:51:25 +0200

    Add coverage supplement for atomic-*.
    These are for the module declaration, and the inernal datastructures
    declaration.

--- tests/unit/coverage-supplement.scm
+++ tests/unit/coverage-supplement.scm
@@ -10,4 +10,9 @@
 (("module/vcomponent/base.scm"
   "f98a3887020c400595bcc32805f968ebebca685bc1c18ef1f1531f55d9f72ec1"
   73 83 108 1)
- )
+ ("module/hnh/util/atomic-queue.scm"
+  "40c0e0feb77392e3eb1f6ab6136cc116aeeded6030eea7db6688901df5ed021d"
+  1 18)
+ ("module/hnh/util/atomic-stack.scm"
+  "147b45d2216c378c35d5c3ed0228be393b6c287f2a5515802928040f2087378e"
+  1 13 29))
