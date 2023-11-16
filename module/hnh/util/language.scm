965b3b9d0ad3faa337ecdc8db1565e1a5819b13a
Hugo Hörnquist <hugo@lysator.liu.se>
2023-11-16 03:02:07 +0100

    Empty env vars equals unset when resolving language.

--- module/hnh/util/language.scm
+++ module/hnh/util/language.scm
@@ -1,6 +1,12 @@
 (define-module (hnh util language)
   :export (resolve-language))
 
+;;; Given a #f or a string, return #f if #f or an empty string is given,
+;;; and the given string otherwise
+(define (falsify-string s)
+  (cond ((and (boolean? s) (not s)) s)
+        ((string-null? s) #f)
+        (else s)))
 
 ;; Locale objects, such as %global-locale, doesn't provide a way to access the language name,
 ;; This is for procedures which want to handle their translations manually.
@@ -8,7 +14,7 @@
   "Returns a two character symbol representing the \"current\" language. e.g. en"
   (string->symbol
    (string-take
-    (or (getenv "LC_MESSAGES")
-        (getenv "LC_ALL")
+    (or (falsify-string (getenv "LC_MESSAGES"))
+        (falsify-string (getenv "LC_ALL"))
         "en")
     2)))
