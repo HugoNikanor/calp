8489d6406460d93d7a55f49dcaa79ee3352a43b0
Hugo Hörnquist <hugo@lysator.liu.se>
2023-11-16 02:59:54 +0100

    Add some tests for exception utilities.

---
+++ tests/unit/util/exceptions.scm
@@ -0,0 +1,24 @@
+(define-module (test exceptions)
+  :use-module (srfi srfi-64)
+  :use-module (srfi srfi-64 test-error)
+  :use-module (srfi srfi-88)
+  :use-module (hnh util exceptions))
+
+(test-group "Warnings"
+ (parameterize ((warning-handler (lambda (fmt . args)
+                                   (test-equal "Test: ~a" fmt)
+                                   (test-equal '(1) args)))
+                (warnings-are-errors #f))
+   (warning "Test: ~a" 1))
+
+ (parameterize ((warning-handler (lambda (fmt . args)
+                                   (test-equal "~a happened" fmt)
+                                   (test-equal '(2) args)))
+                (warnings-are-errors #t))
+   (test-error 'warning
+     (warning "~a happened" 2))))
+
+(test-error "Unreachable"
+  'unreachable (unreachable "procedure" "fmt" (list)))
+
+'((hnh util exceptions))
