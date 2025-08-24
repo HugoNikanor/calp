86b7a27749b2e0490c6c05baf53d44ec4dc45d1a
Hugo Hörnquist <hugo@lysator.liu.se>
2025-08-24 18:14:40 +0200

    Add sample usage of webdav entry point.

---
+++ tests/more-webdav.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Start a webdav server, and check if it behaves with some weird
+# files.
+
+root=$(dirname "$(dirname "$(realpath "$0")")")
+eval "$(env __PRINT_ENVIRONMENT=1 "${root}/calp")"
+
+tmpdir=$(mktemp --directory)
+export tmpdir
+
+config_file=$(mktemp)
+
+cat - > "$config_file" <<-"EOF"
+(use-modules (calp config-base))
+
+((@ (calp entry-points webdav) webdav-resources)
+  `(("/files" file
+      path: ,(getenv "tmpdir"))
+    ("/virtual" virtual
+      content: "Hello, World\n")
+   )
+)
+EOF
+
+# configure weird files
+echo 'File contents' > "$tmpdir/file"
+if [ "$(id -u)" = 0 ]; then
+    mknod char c 0 0
+    mknod block b 0 0
+fi
+mkdir "$tmpdir/directory"
+ln -s file "$tmpdir/working-symlink"
+ln -s nonexistance "$tmpdir/broken-symlink"
+mkfifo "$tmpdir/fifo"
+"${root}/scripts/mksock" "$tmpdir/socket"
+
+portpipe=$(mktemp --dry-run)
+mkfifo "$portpipe"
+"$root/calp" webdav --config "$config_file" --randport "$portpipe" &
+
+port=$(cat "$portpipe")
+
+curl -X PROPFIND -H 'Depth: Infinity' "http://localhost:$port" \
+    | xmllint --format - \
+    | highlight -S xml
+
+rm "$config_file"
+rm "$portpipe"
+# rm -r "$tmpdir"
+# TODO clean up test directory
