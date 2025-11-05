e83f74cb0a14abad5d1fcb087a0aaa2518f94c53
Hugo Hörnquist <hugo@lysator.liu.se>
2025-11-05 12:38:54 +0100

    Introduce calp xml namespace.
    This is currently not used, but will later be used for WebDAV attributes
    introduced by us.

--- module/calp/namespaces.scm
+++ module/calp/namespaces.scm
@@ -7,6 +7,7 @@
 (define-public webdav (string->symbol "DAV:"))
 (define-public caldav (string->symbol "urn:ietf:params:xml:ns:caldav"))
 (define-public xcal   (string->symbol "urn:ietf:params:xml:ns:icalendar-2.0"))
+(define-public calp-namespace (string->symbol "http://hugo.hornquist.se/namespaces/calp"))
 
 (define-public namespaces
   `((d . ,webdav)
