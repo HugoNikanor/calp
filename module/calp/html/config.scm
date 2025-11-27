374e42829f3f8ee9bc8993b2c415d502bc9f0a37
Hugo Hörnquist <hugo@lysator.liu.se>
2025-11-27 11:57:10 +0100

    Set up (calp html config) module properly.

--- module/calp/html/config.scm
+++ module/calp/html/config.scm
@@ -1,14 +1,11 @@
 (define-module (calp html config)
-  :use-module (hnh util)
-  :use-module (calp util config)
-  :use-module (calp translation)
-  )
+  :use-module (calp util config))
 
 (define-config debug #f
-  description: (_ "Places the generated thingy in debug mode"))
+  description: (G_ "Places the generated thingy in debug mode"))
 
 
 ;;; NOTE edit mode should preferably depend on login-status of the user
 ;;; but this works for the time being.
 (define-config edit-mode #t
-  description: (_ "Makes the document editable"))
+  description: (G_ "Makes the document editable"))
