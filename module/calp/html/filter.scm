284ce5f9bdc9a9d8f6f4a23cb59778e7dc3aea37
Hugo Hörnquist <hugo@lysator.liu.se>
2025-12-17 05:26:53 +0100

    Document (calp html filter) configuration parameters.

--- module/calp/html/filter.scm
+++ module/calp/html/filter.scm
@@ -1,9 +1,14 @@
 (define-module (calp html filter)
   :use-module (calp util config)
+  :use-module (calp translation)
   )
 
 (define-config summary-filter (lambda (_ a) a)
-  pre: (ensure procedure?))
+  pre: (ensure procedure?)
+  description:
+  (G_ "Transforms the event summary in HTML output"))
 
 (define-config description-filter (lambda (_ a) a)
-  pre: (ensure procedure?))
+  pre: (ensure procedure?)
+  description:
+  (G_ "Transforms the description body in HTML output."))
