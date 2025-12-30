8c4a5a93ce643c47fb2f277a2fbe8b1a12715513
Hugo Hörnquist <hugo@lysator.liu.se>
2025-12-30 03:00:20 +0100

    Add some TODOs to system/config.scm.

--- system/config.scm
+++ system/config.scm
@@ -1,8 +1,14 @@
+;;; Commentary:
+;;; This is (in theory) a configuration file suitable as a system wide
+;;; default for when installing calp as a package. It's however
+;;; untested, and probably not suitable for use at all.
+;;; Code:
 (use-modules (calp util config)
              (ice-9 regex)
              ((datetime) :select (mon))
              (glob))
 
+;;; TODO this is replaced with (@ (vcomponent config) data-stores)
 ((@ (vcomponent) calendar-files) (glob "/var/lib/calp/.local/var/cal/*"))
 
 (define (parse-links str)
