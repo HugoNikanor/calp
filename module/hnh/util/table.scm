2cc74c2c65090be684a0918201c5a2f466e0251b
Hugo Hörnquist <hugo@lysator.liu.se>
2025-12-31 11:18:09 +0100

    Change table internals to actually be called table.

--- module/hnh/util/table.scm
+++ module/hnh/util/table.scm
@@ -18,27 +18,27 @@
   :use-module (hnh util named-type)
   :use-module (hnh util destructure)
   :use-module (ice-9 curried-definitions)
-  :export ((make-tree . table)
-           (tree-type . table-type)
-           (tree-of . table-of)
-           (tree-get . table-get)
-           (tree-preview . table-preview)
-           (tree-put . table-put)
-           (tree-remove . table-remove)
-           (tree-pop . table-pop)
-           (tree-any . table-any)
-           (tree->list . table->list)
-           (tree? . table?)
-           (tree-terminal? . table-empty?)
-           (tree-focus . table-focus)
-           (tree-equal? . table-equal?)
-           (tree-diff . table-diff)
-           (serialize-tree . serialize-table)
-           (tree-filter-map . table-filter-map)
-           (tree-union . table-union)
-           (tree-intersection . table-intersection)
-           (tree-difference . table-difference)
-           (alist->tree . alist->table)))
+  :export (table
+           table-type
+           table-of
+           table-get
+           table-preview
+           table-put
+           table-remove
+           table-pop
+           table-any
+           table->list
+           table?
+           (table-terminal? . table-empty?)
+           table-focus
+           table-equal?
+           table-diff
+           serialize-table
+           table-filter-map
+           table-union
+           table-intersection
+           table-difference
+           alist->table))
 
 (define (symbol<? . args)
   (apply string<? (map symbol->string args)))
@@ -46,96 +46,97 @@
 (define-syntax-rule (symbol< args ...)
   (string< (symbol->string args) ...))
 
-(define (serialize-tree t)
-  `(-> (table ,@(if (tree-type t) (list (serialize (tree-type t))) '()))
-       ,@(tree->list t (lambda (k v) `(table-put ,(serialize k) ,(serialize v))))))
+(define (serialize-table t)
+  `(-> (table ,@(if (table-type t) (list (serialize (table-type t))) '()))
+       ,@(table->list t (lambda (k v) `(table-put ,(serialize k) ,(serialize v))))))
 
-(define-type (tree-node
-              serializer: serialize-tree
+(define-type (table-node
+              serializer: serialize-table
               constructor: (lambda (constructor type-check)
                              (lambda* (key: key value type
-                                            (left (tree-terminal type: type))
-                                            (right (tree-terminal type: type)))
+                                            (left (table-terminal type: type))
+                                            (right (table-terminal type: type)))
                                (type-check key value left right type)
                                (when type
                                  (typecheck value ((named-type-type type))
-                                            "tree-node" (named-type-name type)))
+                                            "table-node" (named-type-name type)))
                                (constructor key value left right type))))
   (key type: symbol?)
   value
-  (left type: tree? default: (tree-terminal))
-  (right type: tree? default: (tree-terminal))
+  (left type: table? default: (table-terminal))
+  (right type: table? default: (table-terminal))
   (node-type type: (or false? named-type?) default: #f keyword: type))
 
-;; Tree node without content. Holds a reference to the type, which it
+;; Table node without content. Holds a reference to the type, which it
 ;; will share when added to
-(define-type (tree-terminal
+(define-type (table-terminal
               serializer:
-              (lambda (t) `(table ,@(if (tree-type t) (list (serialize (tree-type t))) '()))))
+              (lambda (t) `(table ,@(if (table-type t) (list (serialize (table-type t))) '()))))
   (terminal-type type: (or false? named-type?) default: #f keyword: type))
 
-;; Wrapped for better error messages
-(define* (make-tree optional: type) (tree-terminal type: type))
+;; Wrapped to handle type as optional argument, while allowing
+;; both inner and leaf nodes to be constructed as (proc type: type).
+(define* (table optional: type) (table-terminal type: type))
 
-(define (tree? x)
-  (or (tree-node? x)
-      (tree-terminal? x)))
+(define (table? x)
+  (or (table-node? x)
+      (table-terminal? x)))
 
 
 
-(define* (tree-type x optional: (v (nothing)))
+(define* (table-type x optional: (v (nothing)))
   (typecheck v optional?)
   (define accessor
-    (cond ((tree-node? x) node-type)
-          ((tree-terminal? x) terminal-type)))
+    (cond ((table-node? x) node-type)
+          ((table-terminal? x) terminal-type)))
   (if (just? v)
       (accessor x (from-just v))
       (accessor x)))
 
 
-(define-syntax-rule (tree-of var inner)
-  (and (tree? var)
-       (tree-type var)
+(define-syntax-rule (table-of var inner)
+  (and (table? var)
+       (table-type var)
        ;; NOTE this unfortunately breaks type aliasing.
        ;; Figure out how to expand
        (equal? (quote inner)
-               (named-type-name (tree-type var)))))
+               (named-type-name (table-type var)))))
 
 
 ;; Lens for focusing a specific entry in a table.
-(define (((tree-focus k) tree) op)
-  (cond ((tree-terminal? tree)
+(define (((table-focus k) table) op)
+  (cond ((table-terminal? table)
          (let ((ret (op (nothing))))
            (cond ((just? ret)
-                  (tree-node key: k value: (from-just ret)
-                             type: (tree-type tree)))
-                 ((nothing? ret) (tree-terminal type: (tree-type tree)))
-                 (else (scm-error 'misc-error "tree-focus"
-                                  "Non-wrapped value returned to tree-focus: ~s"
+                  (table-node key: k value: (from-just ret)
+                              type: (table-type table)))
+                 ((nothing? ret) (table-terminal type: (table-type table)))
+                 (else (scm-error 'misc-error "table-focus"
+                                  "Non-wrapped value returned to table-focus: ~s"
                                   (list ret)
                                   #f)))))
-        ((eq? k (key tree))
-         (let ((ret (op (just (value tree)))))
-           (cond ((just? ret) (value tree (from-just ret)))
-                 ((nothing? ret) (tree-type (merge-trees (left tree) (right tree))
-                                            (just (tree-type tree))))
-                 (else (scm-error 'misc-error "tree-focus"
-                                  "Non-wrapped value returned to tree-focus: ~s"
+        ((eq? k (key table))
+         (let ((ret (op (just (value table)))))
+           (cond ((just? ret) (value table (from-just ret)))
+                 ((nothing? ret) (table-type (merge-tables (left table) (right table))
+                                             (just (table-type table))))
+                 (else (scm-error 'misc-error "table-focus"
+                                  "Non-wrapped value returned to table-focus: ~s"
                                   (list ret)
                                   #f)))))
         (else
-         (modify tree (lens-compose (if (symbol<? k (key tree))
-                                        left* right*)
-                                    (tree-focus k))
+         (modify table (lens-compose (if (symbol<? k (key table))
+                                         left* right*)
+                                     (table-focus k))
                  op))))
 
-(define* (tree-equal? a b optional: (comperator equal?))
-  (or (and (tree-terminal? a) (tree-terminal? b))
-      (let loop ((as (tree->list a))
+(define* (table-equal? a b optional: (comperator equal?))
+  (or (and (table-terminal? a) (table-terminal? b))
+      (let loop ((as (table->list a))
                  (b b))
         (if (null? as)
-            (tree-terminal? b)
-            (let ((v rest (tree-pop b (caar as))))
+            (table-terminal? b)
+            (let ((v rest (table-pop b (caar as))))
               (and (not (nothing? v))
                    (comperator (from-just v) (cdar as))
                    (loop (cdr as) rest)))))))
@@ -143,17 +144,17 @@
 ;;; Returns a list of diff-objects, where diff objects on the form
 ;;; '(absent ,table ,key) ; where table is the symbol 'a or 'b
 ;;; '(diff ,key ,a-value ,b-value)
-(define* (tree-diff a b optional: (comperator equal?))
-  (if (and (tree-terminal? a) (tree-terminal? b))
+(define* (table-diff a b optional: (comperator equal?))
+  (if (and (table-terminal? a) (table-terminal? b))
       '()
-      (let loop ((as (tree->list a))
+      (let loop ((as (table->list a))
                  (b b))
-        (cond ((and (null? as) (tree-terminal? b)) '())
+        (cond ((and (null? as) (table-terminal? b)) '())
               ((null? as)
                (map (lambda (p) `(absent a ,(car p)))
-                    (tree->list b)))
+                    (table->list b)))
               (else
-               (let ((v rest (tree-pop b (caar as))))
+               (let ((v rest (table-pop b (caar as))))
                  (cond ((nothing? v)
                         (cons `(absent b ,(caar as))
                               (loop (cdr as) rest)))
@@ -164,112 +165,112 @@
                               (loop (cdr as) rest))))))))))
 
 
-;;; TODO rename to `tree-ref`?
-(define* (tree-get tree k optional: default)
-  (unjust (get tree (tree-focus k)) default))
+;;; TODO rename to `table-ref`?
+(define* (table-get table k optional: default)
+  (unjust (get table (table-focus k)) default))
 
-(define (tree-preview tree k)
-  (get tree (tree-focus k)))
+(define (table-preview table k)
+  (get table (table-focus k)))
 
-(define (tree-remove tree k)
-  (set tree (tree-focus k) (nothing)))
+(define (table-remove table k)
+  (set table (table-focus k) (nothing)))
 
 ;;; Returns 2 values
 ;;; - maybe (the focused value)
 ;;; - the table without that key
-(define (tree-pop tree k)
+(define (table-pop table k)
   (let ((result (nothing)))
-    (let ((resulting-tree
-           (modify tree (tree-focus k)
+    (let ((resulting-table
+           (modify table (table-focus k)
                    (lambda (m)
                      (set! result m)
                      (nothing)))))
-      (values result resulting-tree))))
+      (values result resulting-table))))
 
-;;; Return an arbitrary value from the table, alongside the tree without that value
-;;; tree-any :: tree -> (optional x), tree
-(define (tree-any tree)
-  (if (tree-terminal? tree)
-      (values (nothing) tree)
-      (tree-pop tree (key tree))))
+;;; Return an arbitrary value from the table, alongside the table without that value
+;;; table-any :: table -> (optional x), table
+(define (table-any table)
+  (if (table-terminal? table)
+      (values (nothing) table)
+      (table-pop table (key table))))
 
-(define (tree-put tree k v)
-  (set tree (tree-focus k) (just v)))
+(define (table-put table k v)
+  (set table (table-focus k) (just v)))
 
 
-;;; Merge two trees.
+;;; Merge two tables.
 ;;; Note that this discards type information
-(define (merge-trees a b)
+(define (merge-tables a b)
   ;; TODO write a better version of this
-  ;; Possibly one which re-balances the trees
-  (fold (lambda (pair tree)
-          (tree-put tree (car pair) (cdr pair)))
+  ;; Possibly one which re-balances the tables
+  (fold (lambda (pair table)
+          (table-put table (car pair) (cdr pair)))
         a
-        (tree->list b)))
+        (table->list b)))
 
 ;; in-order traversal
-(define* (tree->list tree optional: (proc cons))
-  (if (tree-terminal? tree)
+(define* (table->list table optional: (proc cons))
+  (if (table-terminal? table)
       '()
-      (append (tree->list (left tree) proc)
-              (list (proc (key tree) (value tree)))
-              (tree->list (right tree) proc))))
+      (append (table->list (left table) proc)
+              (list (proc (key table) (value table)))
+              (table->list (right table) proc))))
 
 ;; undefined order, probably pre-order
-(define (tree-filter-map f tree)
-  (let recurse ((tree tree))
-    (if (tree-terminal? tree)
-        tree
-        (destructure (f (key tree) (value tree))
+(define (table-filter-map f table)
+  (let recurse ((table table))
+    (if (table-terminal? table)
+        table
+        (destructure (f (key table) (value table))
           ((just v)
-           (-> tree
+           (-> table
                (set value* v)
                (modify left*  recurse)
                (modify right* recurse)))
           ((nothing)
-           (merge-trees (recurse (left tree))
-                        (recurse (right tree))))))))
+           (merge-tables (recurse (left table))
+                         (recurse (right table))))))))
 
-(define (alist->tree alist)
-  (fold (lambda (kv tree) (tree-put tree (car kv) (cdr kv)))
-        (tree-terminal)
+(define (alist->table alist)
+  (fold (lambda (kv table) (table-put table (car kv) (cdr kv)))
+        (table-terminal)
         alist))
 
 
 
 ;; right-biased
 ;; reversed operand order, since reduce calls (proc elem prev)
-(define (tree-union% b a)
-  (fold (lambda (pair tree)
-          (tree-put tree (car pair) (cdr pair)))
+(define (table-union% b a)
+  (fold (lambda (pair table)
+          (table-put table (car pair) (cdr pair)))
         a
-        (tree->list b)))
+        (table->list b)))
 
-(define (tree-union . as)
-  (typecheck as (list-of tree?))
-  (reduce tree-union% (make-tree) as))
+(define (table-union . as)
+  (typecheck as (list-of table?))
+  (reduce table-union% (table) as))
 
 ;; left-biased
-(define (tree-intersection% a b)
-  (tree-filter-map
-   (lambda (k _) (tree-preview b k))
+(define (table-intersection% a b)
+  (table-filter-map
+   (lambda (k _) (table-preview b k))
    a))
 
-(define (tree-intersection . as)
-  (typecheck as (list-of tree?))
-  (reduce tree-intersection% (make-tree) as))
+(define (table-intersection . as)
+  (typecheck as (list-of table?))
+  (reduce table-intersection% (table) as))
 
-(define (tree-difference a b)
-  (fold (swap tree-remove)
-        a (tree->list b (lambda (k _) k))))
+(define (table-difference a b)
+  (fold (swap table-remove)
+        a (table->list b (lambda (k _) k))))
 
 
 
 
 (define (make-indent depth) (make-string (* 2 depth) #\space))
 
-(define* (print-tree tree optional: (depth 0))
-  (unless (tree-terminal? tree)
-    (format #t "~a- ~s: ~s~%" (make-indent depth) (key tree) (value tree))
-    (print-tree (left tree) (1+ depth))
-    (print-tree (right tree) (1+ depth))))
+(define* (print-table table optional: (depth 0))
+  (unless (table-terminal? table)
+    (format #t "~a- ~s: ~s~%" (make-indent depth) (key table) (value table))
+    (print-table (left table) (1+ depth))
+    (print-table (right table) (1+ depth))))
