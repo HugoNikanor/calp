d237ba19631fed191a600518343376d82c3b8338
Hugo Hörnquist <hugo@lysator.liu.se>
2026-01-25 21:35:41 +0100

    Update vendored pprint to our module tree.

--- module/hnh/util/pprint.scm
+++ module/hnh/util/pprint.scm
@@ -2,6 +2,7 @@
 ;;;;
 ;;;; 	Copyright (C) 2001, 2004, 2006, 2009, 2010,
 ;;;;      2012, 2013, 2014, 2023 Free Software Foundation, Inc.
+;;;     Copyright (C) 2025 Hugo Hörnquist <hugo@lysator.liu.se>
 ;;;; 
 ;;;; This library is free software; you can redistribute it and/or
 ;;;; modify it under the terms of the GNU Lesser General Public
@@ -17,7 +18,7 @@
 ;;;; License along with this library; if not, write to the Free Software
 ;;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 ;;;; 
-(define-module (ice-9 pretty-print)
+(define-module (hnh util pprint)
   #:use-module (ice-9 match)
   #:use-module (srfi srfi-1)
   #:use-module (rnrs bytevectors)
