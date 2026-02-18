e9c1ee7b333c9de4164091a1f193e7de3d5716e6
Hugo Hörnquist <hugo@lysator.liu.se>
2026-02-18 21:42:27 +0100

    Add content type negotiation to the calendar-object resource.

--- module/calp/webdav/resource/calendar-object.scm
+++ module/calp/webdav/resource/calendar-object.scm
@@ -45,52 +45,70 @@
   ;; - is part of the supported-calendar-component-set?
   ;; - contains proper timezone info?
   ;; - only contains one calendar resource?
+
   )
 
+;;; TODO be sure to set a "Vary: accept" header on the resoponse
 
-;;; TODO gather this set from the module system
-(define content-types
-  (alist->table
-   (map (lambda (media-type)
-          (cons (string->symbol (format #f "~a/~a" (car media-type) (cdr media-type)))
-                (module-ref (resolve-interface `(vcomponent media-type ,@media-type))
-                            'format)))
-        '((text calendar)
-          (application calendar+xml)
-          (application calendar+json)))))
+;; TODO does an HTTP response header informing the client of
+;; alternative representations exist? E.g. when being sent text/calendar,
+;; the client is informed of the existance of application/calendar+xml
+;; and so on.
+
+(define provided-content-types
+  (let ()
+    (define (get type) (module-ref (resolve-interface `(vcomponent media-type ,@type))
+                                   'format))
+    ;; TODO gather this set from the module system
+    `((text
+       (calendar . ,(get '(text calendar))))
+      (application
+       (calendar+xml . ,(get '(application calendar+xml)))
+       (calendar+json . ,(get '(application calendar+json)))))))
+
+(define (parse-media-type-identifier-symbol symb)
+  ;; TODO possibly assert that return is of exactly length 2
+  (map string->symbol (string-split (symbol->string symb) #\/)))
+
+(define (negotiate-content-type client-accept)
+  ;; (typecheck client-accept (or false? (list-of (pair-of symbol? (alist-of symbol? any-type)))))
+
+  ;; NOTE this ignores any */*, meaning that one of those with q higher than something else
+  ;; will never be used.
+  ;; NOTE this uses the caputured provided-content-types, which is an
+  ;; alist of major types to an alist of minor types to media-type format
+  ;; objects. Each sub-alist MUST have at least one element.
+  ;; Both the inner and outeor sub-list are sorted in the order the server prefers them.
+  (or (let loop ((accepted-media-types
+                  (map parse-media-type-identifier-symbol
+                       (map car       ; we don't care about the q any longer
+                            (sort*
+                             (or client-accept '())
+                             > (lambda (p) (or (assoc-ref (cdr p) 'q) 1000)))))))
+        (and (not (null? accepted-media-types))
+             (let ((accepted-media-type (car accepted-media-types)))
+              (and=> (assoc-ref provided-content-types (car accepted-media-type))
+                     (lambda (content-type-group)
+                       (if (eq? '* (cadr accepted-media-type))
+                           (cdar content-type-group)
+                           (assoc-ref content-type-group (cadr accepted-media-type))))))))
+
+      (cdr (car (cdr (car provided-content-types))))))
 
 (define-method (content-type (resource <calendar-object-resource>) headers)
-  "text/calendar")
+  (media-type (negotiate-content-type (assoc-ref headers 'accept))))
 
 (define-method (content (resource <calendar-object-resource>) headers)
-  ;; Retrive content from underlying store
-  ;; Ensure that METHOD is unset
-  ;; Serialize it depending on the `Accept` header.
-  ;; Generate appropriate etags
-  ;; Set appropirate content-type
-  ;; send off content
-
-  ;; Sort accept by quality (undefined = 1)
-  ;; Find first content type which matches what we provide
-  ;; if no match, return default
-
-  (define content-type
-    (or
-     (find (lambda (ct) (table-get content-types ct))
-           (map car
-                (sort*
-                 (or (assoc-ref headers 'accept) '((*/*)))
-                 > (lambda (p) (or (assoc-ref (cdr p) 'q) 1000)))))
-     'text/calendar))
+  (define media-format
+    (negotiate-content-type (assoc-ref headers 'accept)))
 
   (values
    (call-with-output-string
      (lambda (port)
-       ((serializer (table-get content-types content-type))
+       ((serializer media-format)
         (get-by-href (data-store resource) (href resource))
         port)))
-   (symbol->string content-type))
-  )
+   (media-type media-format)))
 
 (define-method (dead-properties (_ <calendar-object-resource>))
   '())
