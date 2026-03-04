3e5ddf6fbc6b9444e65c7b159b5bd16106620f4c
Hugo Hörnquist <hugo@lysator.liu.se>
2026-03-04 19:08:57 +0100

    Update sample-webdav config to aid in debugging.
    This adds a number of index pages to allow quicker locating of different
    pages. It also changes available collections to a set slightly more
    representative of what is to be tested. However, this calendar-home set
    is still far from perfect, and way to hard coded.

--- sample-webdav.scm
+++ sample-webdav.scm
@@ -7,34 +7,79 @@
 
 (use-modules (calp config-base)
              (scheme base)
-             (hnh util path))
+             (hnh util path)
+             (srfi srfi-267))
 
+
+(define root-content #"html"
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Hello!</title>
+  </head>
+  <body>
+    <h1>Hello!</h1>
+    <ul>
+      <li><a href="/explorer/">WebDAV explorer of this domain</a>
+      <li><a href="/users/">User list</a>
+    </ul>
+  </body>
+</html>
+  "html")
+
+(define user-list-content #"html"
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>User list</title>
+  </head>
+  <body>
+    <h1>User list</h1>
+    <ul>
+      <li><a href="hugo/">hugo</a>
+    </ul>
+  </body>
+</html>
+  "html")
 
 ((@ (calp entry-points webdav) webdav-resources)
- `(virtual (; ("files" (file path: ,(getenv "HOME")))
-            ; ("virtual" (virtual content: ,(string->utf8 "Test string\n")))
-            ("explorer"
-             (file
-              path: ,(path-append (dirname (current-filename)) "webdav-explorer")))
+ `(virtual
+   content: ,(string->utf8 root-content)
+   content-type: "text/html; charset=UTF-8"
+   (
+    ;; ("files" (file path: ,(getenv "HOME")))
+    ;; ("virtual" (virtual content: ,(string->utf8 "Test string\n")))
+    ("explorer" (file path: ,(path-append (dirname (current-filename)) "webdav-explorer")))
 
-            ("users"
-             ;; TODO user list should be its own resource
-             ;; type, instead of a hard coded list of users here.
-             (virtual
-              (("hugo"
-                (principal
-                 (("calendars"
-                   (calendar-home
-                    content-type: "text/plain"
-                    content: ,(string->utf8 "This is the calendar home\n")
-                    (("sqlite"
-                      (calendar-collection "store://sqlite/tmp/calendar.db"))
-                     ("calendar.ics"
-                      (calendar-collection "store://file/tmp/calendar.ics?media=text/calendar"))
-                     ("vdir"
-                      (calendar-collection "store://vdir/tmp/Calendar.vdir?media=text/calendar"))
-                     ;; ("calendar.xcs"
-                     ;;  (calendar-collection "store://file/tmp/calendar.xcs?media=application/calendar+xml"))
-                     ;; ("calendar.json"
-                     ;;  (calendar-collection "store://file/tmp/calendar.json?media=application/calendar+json"))
-                     ))))))))))))
+    ("users"
+     ;; TODO user list should be its own resource
+     ;; type, instead of a hard coded list of users here.
+     (virtual
+       content: ,(string->utf8 user-list-content)
+       content-type: "text/html; charset=UTF-8"
+       (("hugo"
+         (calendar-home
+          (
+           ("rfc-sqlite" (calendar-collection "store://sqlite/tmp/rfc.db"))
+           ("rfc-vdir" (calendar-collection "store://vdir/tmp/rfc-vdir?media=text/calendar"))
+           ("rfc-rfc" (calendar-collection "store://webdav-report-xml/home/hugo/code/calp/tests/unit/rfc4791/webdav-report.xml"))
+           )
+          )))
+       ;; (("hugo"
+       ;;   (principal
+       ;;     (("calendars"
+       ;;       (calendar-home
+       ;;         content-type: "text/plain"
+       ;;         content: ,(string->utf8 "This is the calendar home\n")
+       ;;         (("sqlite"
+       ;;           (calendar-collection "store://sqlite/tmp/calendar.db"))
+       ;;          ("calendar.ics"
+       ;;           (calendar-collection "store://file/tmp/calendar.ics?media=text/calendar"))
+       ;;          ("vdir"
+       ;;           (calendar-collection "store://vdir/tmp/Calendar.vdir?media=text/calendar"))
+       ;;          ;; ("calendar.xcs"
+       ;;          ;;  (calendar-collection "store://file/tmp/calendar.xcs?media=application/calendar+xml"))
+       ;;          ;; ("calendar.json"
+       ;;          ;;  (calendar-collection "store://file/tmp/calendar.json?media=application/calendar+json"))
+       ;;          )))))))
+       )))))
